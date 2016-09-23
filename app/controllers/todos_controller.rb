b1e0e108530961d9b340aa8be8d729c704b25c19
Jordan Lawes <jordan.lawes@gmail.com>
2016-09-23 20:24:44 +0000

    Initialized Repo add

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,32 @@
+class TodosController < ApplicationController
+    def index
+        @todos - Todo.all
+    end
+    
+    def new
+        @todo = Todo.new
+    end
+    
+    def create
+        @todo = Todo.create(todo_params)
+        redirect_to roddos_path
+    end
+    
+    private
+    def todo_params
+        params.require(:todo).permit(:item)end
+        
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+    
+    def update
+      @todo = Todo.find(params[:id]) 
+      @todo.update
+    end
+    
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+    end
+end
